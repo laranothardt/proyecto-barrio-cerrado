3d5ad773befc7a643606fe5be4d8a3cc740bc1b4
laranothardt <laranothardt@gmail.com>
2026-08-18 10:44:46 -0300

    Inicio de sesion y redireccion

--- Grupo7_BarrioCerradoII/Administradores.aspx.cs
+++ Grupo7_BarrioCerradoII/Administradores.aspx.cs
@@ -13,12 +13,19 @@ namespace Grupo7_BarrioCerradoII
         {
             if (!IsPostBack)
             {
-                if (Session["Rol"] == null || Session["Rol"].ToString() != "Administrador")
+                if (Session["Rol"] == null)
+                {
+                    Response.Redirect("~/IniciarSesion.aspx");
+                    return;
+                }
+
+                string rol = Session["Rol"].ToString();
+
+                if (rol != "3" && rol != "Administrador")
                 {
                     Response.Redirect("~/Default.aspx");
                     return;
                 }
-
             }
         }
     }
